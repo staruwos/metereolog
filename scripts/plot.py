fd0e994fd84a55c2d53ae83390ef5cb2f601dd94
Staruwos <juliotrasferetti@gmail.com>
2019-03-05 09:27:31 -0300

    update

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,3 +1,4 @@
+import sys
 import matplotlib.pyplot as PlotLib
 import sqlite3
 
@@ -10,16 +11,32 @@ def search_day(cursor):
     for row in rows:
         print row
 
+def hour_range(rows, cursor):
+    print("Aopa") 
+
 def search_month_range(x, y = -1):
     if y == -1:
         print(x)
     else:
         print (x, y)
 
-connection = sqlite3.connect("metereolog.db")
-cursor = connection.cursor()
+#connection = sqlite3.connect("metereolog.db")
+#cursor = connection.cursor()
 
-search_day(cursor)
+#search_day(cursor)
+
+def main():
+    args = list(sys.argv)
+    n_args = len(sys.argv)
+    
+    if n_args >= 2:
+        if args[1] == "--range":
+            print n_args
+            if n_args >= 4:
+                print (args[2] + ", " + args[3])
+
+if __name__ == "__main__":
+    main()
 
 #cursor.execute("SELECT * FROM infos")
 
